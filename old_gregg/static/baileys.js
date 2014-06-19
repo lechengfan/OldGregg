bb41cf0bb1fc1f9dbc23714574ef8452eb0b1eb1
Ian Char <ianchar4@gmail.com>
2014-06-19 00:07:16 -0600

    Made a few tweeks

--- old_gregg/static/baileys.js
+++ old_gregg/static/baileys.js
@@ -9,15 +9,15 @@ var clicked = 0;
  $('#shoePic').click(function() {
     switch(clicked) {
         case 0:
-         	$("#baileys").animate({top: "7em"}, 1000);
+         	$("#baileys").animate({top: "12em"}, 1000);
             clicked++;
             break;
         case 1:
-            $("#baileys").animate({top: "14em"}, 1000);
+            $("#baileys").animate({top: "24em"}, 1000);
             clicked++;
             break;
         case 2:
-            $("#baileys").animate({top: "21em"}, 1000);
+            $("#baileys").animate({top: "33em"}, 1000);
             clicked++;
             break;
         default: 
