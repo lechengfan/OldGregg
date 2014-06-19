bb888e5d0386ef3f469459e4d9013c3a86bce348
Ian Char <ianchar4@gmail.com>
2014-06-18 18:38:02 -0600

    Changed phrase

--- old_gregg/baileys/views.py
+++ old_gregg/baileys/views.py
@@ -5,6 +5,6 @@ from django.shortcuts import render_to_response
 def index(request):
 	context = RequestContext(request)
 
-	context_dict = {'boldmessage': "Wanna Drink Baileys Out of a Shoe?"}
+	context_dict = {'boldmessage': "Ever drunk Bailey's from a shoe?"}
 	
 	return render_to_response('baileys/index.html', context_dict, context)
