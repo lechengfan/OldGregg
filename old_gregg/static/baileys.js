798c9c5bb13fdf4c21c43538a5fad0a9eee7ed0e
Tommy Fan <lecheng.fan@stanford.edu>
2014-06-21 16:41:16 -0600

    audio now works for drinking and refilling

--- old_gregg/static/baileys.js
+++ old_gregg/static/baileys.js
@@ -1,12 +1,8 @@
 //Animations for the Bailey's drink
 var clicked = 0;
-var gulp = document.getElementById("gulp");
-var fill = document.getElementById("fill");
 
 $(document).ready(function() {
     $('h1').hide().fadeIn(5000);
-    gulp.load();
-    fill.load();
 });
 
 var drink = function() {
@@ -31,14 +27,12 @@ var refill = function() {
             $("#baileys").animate({top: "33em"}, 1000);
             clicked++;
             break;
-        default: 
-            $("#baileys").animate({top: "0em"}, 1000);
-             clicked = 0;
+
     }
 });
 
 $('button').click(function() {
-   	$("#baileys").animate({top: "0em"}, 1000);
+   	$("#baileys").animate({top: "0em"}, 2000);
     clicked = 0;
 });
 
